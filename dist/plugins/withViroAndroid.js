a3016c11fafb07e08953aa1783ae72e026a144d3
tsnguyenducphuong <ts.nguyenducphuong@gmail.com>
2023-11-05 00:48:03 +0700

    update regex

--- dist/plugins/withViroAndroid.js
+++ dist/plugins/withViroAndroid.js
@@ -86,10 +86,15 @@ const withViroProjectBuildGradle = (config) => (0, config_plugins_1.withProjectB
     return newConfig;
 });
 const withViroAppBuildGradle = (config) => (0, config_plugins_1.withAppBuildGradle)(config, async (config) => {
+    //config.modResults.contents = config.modResults.contents.replace(/implementation "com.facebook.react:react-native:\+"  \/\/ From node_modules/, `implementation "com.facebook.react:react-native:+"  // From node_modules
     //config.modResults.contents = config.modResults.contents.replace(/dependencies\s?{\n\s*\/\/ The version of react-native is set by the React Native Gradle Plugin/, `dependencies {
     //config.modResults.contents = config.modResults.contents.replace(/implementation "com.facebook.react:react-native:*"/, `implementation "com.facebook.react:react-native:0.72.6"  // From node_modules
-    config.modResults.contents = config.modResults.contents.replace(/implementation("com.facebook.react:react-android")/, `implementation("com.facebook.react:react-android")"  
-    
+    //config.modResults.contents = config.modResults.contents.replace(/implementation("com.facebook.react:react-android")/, `implementation("com.facebook.react:react-android")"  
+    //// The version of react-native is set by the React Native Gradle Plugin
+    config.modResults.contents = config.modResults.contents.replace(/\/\/ The version of react-native is set by the React Native Gradle Plugin/, `
+    // The version of react-native is set by the React Native Gradle Plugin
+
+
     compile project(':gvr_common')
     compile project(':arcore_client')
     compile project(path ':react_viro')
